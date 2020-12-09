bc9a8c23b9c2e978c2ff0162d1471bee07a46004
Eduardo Lodi Marzano <lodi001@uol.com.br>
2020-12-09 16:50:33 -0300

    Adde Star and Pipe modules to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {CourseListComponent} from './courses/course-list.component';
-import {StarComponent} from './star/star.component';
-import {ReplacePipe} from './pipe/replace.pipe';
+import {AppComponent} from './app.component';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {Error404Component} from './error-404/error-404.component';
-import {CourseInfoComponent} from './courses/course-info.component';
 import {CourseModule} from './courses/course.module';
 
 @NgModule({
